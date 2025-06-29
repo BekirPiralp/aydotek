e805b71c0c0a9231e8298e26e1f25c7e5de7b8de
Bekir PİRALP <bekir01piralp@gmail.com>
2025-06-29 03:15:29 +0300

    backgorund kısmı ayarlandı temel yapı oluşturuluyor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component , ViewEncapsulation} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class App {
   protected title = 'aydotek';
+
+  protected background_image_url = "none"//"/assets/background/space.jpg";
+
+  ngOnInit(){
+    document.documentElement.style.setProperty('--background-image-url',`url("${this.background_image_url}")`)
+  }
+
 }
+
