e8dc562cc7458f3a47716d44fb3f6d744892fd5b
Bekir PİRALP <bekir01piralp@gmail.com>
2025-07-03 03:32:01 +0300

    routes ayarlarndı

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { Home } from '../components/home/home';
-import { Notfound } from '../components/notfound/notfound';
+import { Home } from '../components/page-components/home-page/main/home/home';
+import { Error } from '../components/page-components/error-page/main/error/error';
 
 export const routes: Routes = [
     {path:'',component: Home},
     {path:'#',component: Home},
-    {path:'**',component: Notfound, pathMatch:'full'}
+    {path:'**',component: Error, pathMatch:'full'}
 ];
