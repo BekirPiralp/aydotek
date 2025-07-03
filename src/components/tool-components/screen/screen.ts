c49222d347c5fa24062015accbea49a7e7ef1011
Bekir PİRALP <bekir01piralp@gmail.com>
2025-07-03 03:32:46 +0300

    wareframe deki belirlenen ekran boyutu için screen oluşturuldu

---
+++ src/components/tool-components/screen/screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tool-screen',
+  imports: [],
+  templateUrl: './screen.html',
+  styleUrl: './screen.css'
+})
+export class Screen {
+
+}
