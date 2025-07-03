783ccc4ff61edb9d430ab4c829de8a61812cee03
Bekir PİRALP <bekir01piralp@gmail.com>
2025-07-04 02:36:59 +0300

    scren kısmı home page in kendisine alındı olması gereken yere

--- src/components/page-components/home-page/main/home/home.ts
+++ src/components/page-components/home-page/main/home/home.ts
@@ -5,10 +5,11 @@ import { SectionAnasayfa } from "../../others/section-anasayfa/section-anasayfa"
 import { SectionHizmetler } from "../../others/section-hizmetler/section-hizmetler";
 import { SectionUrunler } from "../../others/section-urunler/section-urunler";
 import { SectionIletisim } from "../../others/section-iletisim/section-iletisim";
+import { Screen } from "../../../../tool-components/screen/screen";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Footer, SectionAnasayfa, SectionHizmetler, SectionUrunler, SectionIletisim],
+  imports: [Navbar, Footer, SectionAnasayfa, SectionHizmetler, SectionUrunler, SectionIletisim, Screen],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
