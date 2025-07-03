fbac2410071d24b0d24c29488a78f256f398cb87
Bekir PİRALP <bekir01piralp@gmail.com>
2025-07-03 19:40:50 +0300

    hompage yapısı ayarlandı

--- src/components/page-components/home-page/main/home/home.ts
+++ src/components/page-components/home-page/main/home/home.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../../others/navbar/navbar";
+import { Footer } from "../../../defaults/footer/footer";
+import { SectionAnasayfa } from "../../others/section-anasayfa/section-anasayfa";
+import { SectionHizmetler } from "../../others/section-hizmetler/section-hizmetler";
+import { SectionUrunler } from "../../others/section-urunler/section-urunler";
+import { SectionIletisim } from "../../others/section-iletisim/section-iletisim";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Navbar, Footer, SectionAnasayfa, SectionHizmetler, SectionUrunler, SectionIletisim],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
